84da769aa06ce18affa627810b3a1625e355f8ef
Travis Bennett <t_bennett_a@protonmail.com>
2022-06-26 14:44:16 -0400

    Added currency formatter to store

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import formatCurrency from '../utilities/formatCurrency'
 
 // Defined types for StoreItem-could also be an interface since there is no need for any unions or enums
 
@@ -22,7 +23,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemTypes) {
 				<Card.Body className="d-flex flex-column">
 					<Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
 						<span className='fs-2'>{name}</span>
-						<span className='ms-2 text-muted'>{price}</span>
+						<span className='ms-2 text-muted'>{formatCurrency(price)}</span>
 					</Card.Title>
 				</Card.Body>
 		</Card>
