e7ebc71ea7fe616c290949133d5b3b8424784052
Travis Bennett <t_bennett_a@protonmail.com>
2022-06-28 21:11:13 -0400

    Added some information and a modified the layout of the About Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,20 @@
 export function About() {
-    return (
-      <h1>About</h1>
-    )
-  }
-  
+	return (
+		<>
+			<div className="container">
+				<h1 className="d-flex justify-content-center fw-bold">About</h1>
+
+				<p className="pt-5">
+					{" "}
+					This is project is an ecommerce shopping cart built with Vite and
+					coded in React with Typescript and React Bootstrap. It allows the user
+					to select an item(s) of their choosing, the quantity of said item(s),
+					and then allows the user to see the total calculated in the shopping
+					cart. The total is displayed in the users local currency with the
+					corect denomination symbol, e.g $,£, €, ¥, by using React's Intl
+					(International) library.{" "}
+				</p>
+			</div>
+		</>
+	);
+}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,7 +5,7 @@ import {StoreItem} from '../components/StoreItem'
 export function Store() {
 	return (
 		<>
-			<h1>Store</h1>
+			
 			<Row md={2} xs={1} lg={3} className="g-3">
 				{storeItems.map((storeItem) => (
 					<Col key={storeItem.id}><StoreItem {...storeItem}/></Col>
